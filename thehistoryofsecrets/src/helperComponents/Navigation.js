5571b6b37daeb7e100d91e961f5d3520b4456e03
mnemonicmnome <focmycology@gmail.com>
2022-07-04 21:04:13 -0700

    First Experiments with CSS in react

--- thehistoryofsecrets/src/helperComponents/Navigation.js
+++ thehistoryofsecrets/src/helperComponents/Navigation.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import { Outlet, Link } from "react-router-dom";
+import '../styles/Navigation.css'
 
 const Navigation = () => {
+
+    const [showMono, setShowMono] = useState(false);
+
+    const toggleMono = () => {
+        setShowMono(!showMono);
+    }
+
     return(
         <>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="atbash"> Atbash </Link>
-                <Link to="shift"> Shift </Link>
-                <Link to="affine"> Affine </Link>
+            <nav className="Navigation">
+                <Link className="HomeButton" to="/">Home</Link>
+                <div className="DropdownRoot" onClick={toggleMono}>Mono-Alphabetical</div>
+                {showMono ?
+                    (
+                    <ul className="DropdownList">
+                    <li><Link to="atbash"> Atbash </Link></li>
+                    <li><Link to="shift"> Shift </Link></li>
+                    <li><Link to="affine"> Affine </Link></li>
+                    </ul>
+                    ) :
+                    (null)
+                }
             </nav>
             <Outlet/>  
         </>
